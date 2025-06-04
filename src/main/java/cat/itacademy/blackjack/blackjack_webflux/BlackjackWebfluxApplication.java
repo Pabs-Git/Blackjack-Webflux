a709fe325c832ba2acb0f2242f426a95b75cb9e8
Pabs <pabloorbea1@gmail.com>
2025-06-04 12:27:02 +0200

    Fix pom.xml to add all dependencies

---
+++ src/main/java/cat/itacademy/blackjack/blackjack_webflux/BlackjackWebfluxApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.blackjack.blackjack_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/blackjack/blackjack_webflux/BlackjackWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.blackjack.blackjack_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
